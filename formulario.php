8ace8b79fb2a99fed7226aa6cccdb90bc8c96f35
JKarmona <axael371.com@gmail.com>
2025-06-14 10:36:23 -0500

    subir  formulario

---
+++ formulario.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+     <i>
+    <h1>Hello world</h1>
+    </i>
+    <hr>
+    <br>
+    <h2>Hello world</h2>
+    <hr>
+    <h3>Hello world</h3>
+    <hr>
+    <h4>Hello world others</h4>
+    <hr>
+    <h5>Hello world</h5>
+    <form action="" method="post">
+        <label for="">
+            <input type="text" id="Name">
+        cd</label>
+
+        
+    </form>
+</body>
+</html>
